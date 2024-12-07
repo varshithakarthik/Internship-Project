d758f023cc88696d5afa00fffebc8870c775e338
varshithakarthik <82289117+varshithakarthik@users.noreply.github.com>
2024-12-06 20:45:40 -0500

    Add files via upload

---
+++ main/java/com/dev/excelupload/ExcelUploadApplication.java
@@ -0,0 +1,10 @@
+package dev.mj.excelupload;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelUploadApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelUploadApplication.class, args);
+	}
+} 
